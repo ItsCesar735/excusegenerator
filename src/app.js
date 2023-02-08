53e88ef097b21ea2d4bdb889e552bd04b9b47fbb
ItsCesar735 <cesar735@gmail.com>
2023-02-08 00:04:10 +0000

    Generador Creado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  onLoad();
 };
+
+function onLoad() {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let who1 = Math.floor(Math.random() * who.length);
+  let what1 = Math.floor(Math.random() * what.length);
+  let when1 = Math.floor(Math.random() * when.length);
+  document.getElementById("excuse").innerHTML =
+    who[who1] + " " + what[what1] + " " + when[when1];
+}
